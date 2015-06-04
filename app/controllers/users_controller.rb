c6db76b42f577c1a9b795db162f6488610424585
joseph <joseh.john@gmail.com>
2015-06-04 15:27:39 +0300

    A little late push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  .
-  .
-  .
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params) 
     if @user.save
       flash[:success] = "Welcome to the Linking App!"
       redirect_to @user
@@ -14,8 +21,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :password, presence: true, length: {minimum: 6}
+  validates :nickname, presence: true, uniqueness: true
 end
